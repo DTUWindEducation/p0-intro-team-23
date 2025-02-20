11cdc86ba58d65542394e5bdfc8d3c605fc2fd17
Tautvydas <s243092@student.dtu.dk>
2025-02-20 07:13:18 +0100

    added main.py file

---
+++ Preclass_assignments/main.py
@@ -0,0 +1,17 @@
+from functions import greet, goldilocks, square_list, fibonacci_stop, clean_pitch
+#1 Simple function
+greet('world')
+
+#2 If/else statements
+goldilocks(151)
+
+#3 For loops
+square_list([1, 2, 3])
+
+#4 While loops
+fibonacci_stop(30)
+
+#5 Logical operators
+x = [-1, 2, 6, 95]
+status = [1, 0, 0, 0]
+clean_pitch(x, status)
